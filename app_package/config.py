abba7af9d151f231b090abf76e0b5140f1ac1cf6
kevincharley <kevincharley44@gmail.com>
2020-03-05 11:17:56 +0530

    kevin

--- app_package/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-base_dir=os.path.abspath(os.path.dirname(__file__))
-class Config(object):
-    MONGO_URI="mongodb://localhost:27017/datadb"
-    
--- app_package/routes.py
+++
@@ -1,6 +0,0 @@
-from flask import render_template,redirect,url_for,flash
-from app_package import app,mongo
-
-@app.route("/",methods=["GET","POST"])
-def index():
-    
